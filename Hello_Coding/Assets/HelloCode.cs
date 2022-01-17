d8617723946da456f2175844dca394b0ca629482
koo0642 <c34110642@gmail.com>
2022-01-17 12:40:50 +0900

    20220117

--- Hello_Coding/Assets/HelloCode.cs
+++ Hello_Coding/Assets/HelloCode.cs
@@ -6,6 +6,20 @@ public class HelloCode : MonoBehaviour
 {
     void Start()
     {
-        Debug.Log("Hello World!");
+        float distance = Getdistance(2, 2, 5, 6);
+        Debug.Log("(2,2)에서(5,6)까지의 거리 : " + distance);
+
     }
+    float Getdistance(float X1, float Y1, float X2, float Y2)
+    {
+        float width = X2 - X1;
+        float heigth = Y2 - Y1;
+
+        float distance = width * width + heigth * heigth;
+        distance = Mathf.Sqrt(distance);
+
+        return distance;
+    }
+
+
 }
